4709f498cd8675c685f83cbad48aaafe91387b3e
4June03 <nghia2003hlls@gmail.com>
2024-11-19 13:59:52 +0700

    Sử dụng toán tử 3 ngôi trong React để xử lí logic hiển thị

--- src/components/DisplayInfor2.js
+++ src/components/DisplayInfor2.js
@@ -1,24 +1,45 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
+  state = {
+    isShowList: true,
+  };
+
+  handleHideShow = () => {
+    this.setState({
+      isShowList: !this.state.isShowList,
+    });
+  };
+
   render() {
     //Sử dụng destructuring array/object để lấy ra các thuộc tính của props -> tối ưu code nếu truyền nhiều
     const { users } = this.props; //Lấy ra users đã truyền từ lớp cha
+    console.table(users);
     return (
       //props => viet tat cua properties
       //truyen du lieu tu component cha sang con
       <div>
-        {
-          //dùng vòng lặp hiển thị ra list user
-          users.map((user) => {
-            return (
-              <div key={user.id}>
-                <div>My name is {user.name}</div>
-                <div>My age is {user.age}</div>
-              </div>
-            );
-          })
-        }
+        <div>
+          <button onClick={() => this.handleHideShow()}>
+            {this.state.isShowList === true ? "Hide list" : "Show list"}
+          </button>
+        </div>
+
+        {this.state.isShowList && (
+          <div>
+            {
+              //dùng vòng lặp hiển thị ra list user
+              users.map((user) => {
+                return (
+                  <div key={user.id}>
+                    <div>My name is {user.name}</div>
+                    <div>My age is {user.age}</div>
+                  </div>
+                );
+              })
+            }
+          </div>
+        )}
       </div>
     );
   }
