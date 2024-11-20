fef4927a9a622e001343fa38b7afb58a89e6581c
4June03 <nghia2003hlls@gmail.com>
2024-11-20 15:58:12 +0700

    Cách viết functional componet(stateless)

--- src/components/DisplayInfor2.js
+++ src/components/DisplayInfor2.js
@@ -1,50 +1,63 @@
 import React from "react";
 import logo from "./../logo.svg"; //Import ảnh
 
-class DisplayInfor extends React.Component {
-  state = {
-    isShowList: true,
-  };
+// class DisplayInfor extends React.Component {
+//   render() {
+//     //Sử dụng destructuring array/object để lấy ra các thuộc tính của props -> tối ưu code nếu truyền nhiều
+//     const { users } = this.props; //Lấy ra users đã truyền từ lớp cha
+//     console.table(users);
+//     return (
+//       //props => viet tat cua properties
+//       //truyen du lieu tu component cha sang con
+//       <div>
+//         {true && (
+//           <div>
+//             {
+//               //dùng vòng lặp hiển thị ra list user
+//               users.map((user) => {
+//                 return (
+//                   <div key={user.id}>
+//                     <div>My name is {user.name}</div>
+//                     <div>My age is {user.age}</div>
+//                   </div>
+//                 );
+//               })
+//             }
+//           </div>
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
-  handleHideShow = () => {
-    this.setState({
-      isShowList: !this.state.isShowList,
-    });
-  };
-
-  render() {
-    //Sử dụng destructuring array/object để lấy ra các thuộc tính của props -> tối ưu code nếu truyền nhiều
-    const { users } = this.props; //Lấy ra users đã truyền từ lớp cha
-    console.table(users);
-    return (
-      //props => viet tat cua properties
-      //truyen du lieu tu component cha sang con
-      <div>
+//Viết displayinfor dưới dạng functional component
+//Không còn hàm render()
+//không còn this
+//React tự động truyền props
+//ĐƯợc gọi là Stateless vì không có state như class componet
+const DisplayInfor = (props) => {
+  const { users } = props;
+  console.table(users);
+  return (
+    <div>
+      {true && (
         <div>
-          <img src={logo} height="100px" width="100px"></img>
-          <button onClick={() => this.handleHideShow()}>
-            {this.state.isShowList === true ? "Hide list" : "Show list"}
-          </button>
+          {
+            //dùng vòng lặp hiển thị ra list user
+            users.map((user) => {
+              return (
+                <div key={user.id}>
+                  <div>My name is {user.name}</div>
+                  <div>My age is {user.age}</div>
+                  <hr />
+                </div>
+              );
+            })
+          }
         </div>
-
-        {this.state.isShowList && (
-          <div>
-            {
-              //dùng vòng lặp hiển thị ra list user
-              users.map((user) => {
-                return (
-                  <div key={user.id}>
-                    <div>My name is {user.name}</div>
-                    <div>My age is {user.age}</div>
-                  </div>
-                );
-              })
-            }
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+      )}
+    </div>
+  );
+};
 
 export default DisplayInfor;
