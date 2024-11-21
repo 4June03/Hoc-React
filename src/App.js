6a19dce0253876f31e0cc693113398f782e513d4
4June03 <nghia2003hlls@gmail.com>
2024-11-21 20:04:26 +0700

    Cài thêm React router để điều hướng trang, bootstrap và react bootstrap

--- src/App.js
+++ src/App.js
@@ -5,35 +5,8 @@ import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
 import MyComponent from "./components/MyComponent"; //import MyComponent vào App
 import React from "react"; //Để viết class Component thì cần import thư viện React
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app-container">
-        Hello world!
-        {/* sử dụng component như 1 tag đặc biệt của html */}
-        <MyComponent></MyComponent>
-      </div>
-    );
-  }
-}
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello world!
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// }
+const App = () => {
+  return <div className="App">Hello worlds!</div>;
+};
 
 export default App;
