e29be64ae3cb0c0975672ebbd4b414cd32919f51
4June03 <nghia2003hlls@gmail.com>
2024-11-22 00:19:00 +0700

    Sử dụng nested Route, và sử dụng Outlet để hiển thị router con

--- src/App.js
+++ src/App.js
@@ -4,21 +4,18 @@ import { useDispatch, useSelector } from "react-redux";
 import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
 import React from "react"; //Để viết class Component thì cần import thư viện React
 import Header from "./components/header/header";
-import { Link } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      <Header></Header>
-      <div>
-        test link
-        <div>
-          <button>
-            <Link to={"/user"}>go to user page</Link>
-          </button>
-          <button>
-            <Link to={"/admin"}>go to admin page</Link>
-          </button>
+    <div className="app-container">
+      <div className="header-container">
+        <Header></Header>
+      </div>
+      <div className="body-container">
+        <div className="sidenav-container"></div>
+        <div className="content-container">
+          <Outlet />
         </div>
       </div>
     </div>
