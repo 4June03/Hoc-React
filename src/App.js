87ec1b380e8cf9949d4b33a34c8350146d41fe56
4June03 <nghia2003hlls@gmail.com>
2024-11-21 20:29:39 +0700

    chuyển sang nhánh prod, tạo component navbar với react-bootstrap

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from "./logo.svg";
 import "./App.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./components/MyComponent"; //import MyComponent vào App
 import React from "react"; //Để viết class Component thì cần import thư viện React
+import Header from "./components/header/header";
 
 const App = () => {
-  return <div className="App">Hello worlds!</div>;
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
 };
 
 export default App;
