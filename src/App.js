91d4228f14a930b72ac7ca3f2e16597e4234af0a
4June03 <nghia2003hlls@gmail.com>
2024-11-21 21:33:20 +0700

    sử dụng route, router và thẻ Link để điều hướng trang không cần load lại trang

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import { useDispatch, useSelector } from "react-redux";
 import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
 import React from "react"; //Để viết class Component thì cần import thư viện React
 import Header from "./components/header/header";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
       <Header></Header>
+      <div>
+        test link
+        <div>
+          <button>
+            <Link to={"/user"}>go to user page</Link>
+          </button>
+          <button>
+            <Link to={"/admin"}>go to admin page</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
