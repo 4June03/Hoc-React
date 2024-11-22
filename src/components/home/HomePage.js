06f442e7504bf92dd7fa5308155badd8d653da7d
4June03 <nghia2003hlls@gmail.com>
2024-11-22 10:08:10 +0700

    thêm video vào trang home page

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,5 +1,13 @@
+import videoHomePage from "../../assets/hero.mp4";
+
 const HomePage = () => {
-  return <div className="homepage-container">Homepage component</div>;
+  return (
+    <div className="homepage-container">
+      <video autoPlay loop muted width={"500"} height={"500"}>
+        <source src={videoHomePage} type="video/mp4"></source>
+      </video>
+    </div>
+  );
 };
 
 export default HomePage;
